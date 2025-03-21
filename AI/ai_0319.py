22ae917a7b2aa0a06b2eed20e60f6bf99d2caa1d
youngmin109 <qhqo3325@gmail.com>
2025-03-21 16:24:10 +0900

    AIupdate

--- AI/ai_0319.py
+++ AI/ai_0319.py
@@ -1,3 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Data set 
+# input data (features) 
+# H(x) -> input data : x1 -> xn
+x_train = [ np.random.rand() * 10 for _ in range(50) ] # 파이썬에서 list compilation 이 나온 이유
+y_train = [val * np.random.rand() * 5 for val in x_train]
+# y를 Lable 로 표기 
+
+# BGC 배치경사하강법을 이용하여 선형회귀를 적용
+
+print(x_train)
+print("-" * 100)
+print(y_train)
+
+plt.scatter(x_train,y_train, color = "blue")
+plt.show()
+# output data (lable)
+# f(x1) -> f(x2)
